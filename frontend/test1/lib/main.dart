acb1848504837f77187d5f8898b5163a687d52f6
HaNiBaHei <chinnapat2556@hotmail.com>
2023-05-17 14:38:43 +0700

    first commit

---
+++ frontend/test1/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My app",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hehe Boi"),
+        ),
+        body: const Text('Hello World!'),
+      ),
+    );
+  }
+}
+
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+
+    );
+  }
+}
